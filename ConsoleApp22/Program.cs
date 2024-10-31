d7dda5c0669e0c4b05af2ac7ad0c049eabd13c6c
Mesgun <Mesgun@MESGUN->
2024-10-31 10:03:25 +0100

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("github öning");
+            Console.WriteLine("var ska du len<");
+        }
+    }
+}
